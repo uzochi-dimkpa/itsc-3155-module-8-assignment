d25b68f7ef9b0cac411730e2b72d61bc02c8a546
jake-blackb135 <jake@choosecarolinas.com>
2022-03-15 21:22:04 -0400

    Adding part 3

--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -1,1 +1,25 @@
 # TODO: Feature 3
+from src.models.movie import Movie
+from src.repositories.movie_repository import movie_repository_singleton
+
+def test_get_movie_by_title():
+    movie = movie_repository_singleton.create_movie("Up","Pete Docter",10)
+    mov = movie_repository_singleton.get_movie_by_title("Up")
+    assert movie == mov
+
+    mov = movie_repository_singleton.get_movie_by_title("")
+    assert mov == None
+
+    mov = movie_repository_singleton.get_movie_by_title("Down")
+    assert mov == None
+
+    
+
+def test_get_movie_by_title2():
+    movie = movie_repository_singleton.create_movie("Up","Pete Docter",10)
+    movie2 = movie_repository_singleton.create_movie("The Fellowship of the Ring","Peter Jackson",10)
+
+    mov = movie_repository_singleton.get_movie_by_title("The Fellowship of the Ring")
+    assert movie2 == mov
+
+    
